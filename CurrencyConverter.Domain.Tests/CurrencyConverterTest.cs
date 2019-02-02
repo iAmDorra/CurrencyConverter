9c6bbf52a6a7f2020d9fc5e7d143dc8b3173a532
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-02-02 21:57:02 +0100

    Return the same amount if target Currency is same.

---
+++ CurrencyConverter.Domain.Tests/CurrencyConverterTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace CurrencyConverter.Domain.Tests
+{
+    [TestClass]
+    public class CurrencyConverterTest
+    {
+        [TestMethod]
+        public void Should_return_the_same_amount_when_target_currency_is_same()
+        {
+            Currency eurCurrency = new Currency("EUR");
+            Amount eurAmount = new Amount(10, eurCurrency);
+            Converter converter = new Converter();
+
+            Amount convertedAmount = converter.Convert(eurAmount, eurCurrency);
+
+            Amount expectedAmount = new Amount(10, eurCurrency);
+            Check.That(convertedAmount).IsEqualTo(expectedAmount);
+        }
+    }
+}
---
+++ CurrencyConverter.Domain/Converter.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CurrencyConverter.Domain
+{
+    public class Converter
+    {
+        public Amount Convert(Amount amount, Currency targetCurrency)
+        {
+            return amount;
+        }
+    }
+}
