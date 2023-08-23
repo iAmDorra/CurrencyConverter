c3107f9ceacd2d36224f55194d3f469e4f4590ed
Dorra Bartaguiz <dorra.bartaguiz@outlook.com>
2023-08-23 08:49:41 +0200

    Add App project

---
+++ CurrencyConverter.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurrencyConverter.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
